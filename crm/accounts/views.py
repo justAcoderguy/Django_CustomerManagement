0ebce03ebd1b8e6952bb3c1d637a144d84e702df
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-08-09 16:21:32 +0530

    Dynnamic URLs added along with some mods of the customer template

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -23,5 +23,11 @@ def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products' : products})
     
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+
+    context ={'customer': customer, 'orders': orders, 'orders_count' : orders_count}
+
+    return render(request, 'accounts/customer.html', context)
