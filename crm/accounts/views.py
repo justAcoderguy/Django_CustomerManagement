f6c8285aba1925806cd7b63af1bb88c172096ea1
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-08-10 23:43:24 +0530

    Finished implementing all  CRUD features till now

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -44,3 +44,25 @@ def createOrder(request):
 
     context = {'form': form }
     return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance = order)
+    if request.method == 'POST':
+            form = OrderForm(request.POST, instance=order) # setting new post data in that instance of order
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+	order = Order.objects.get(id=pk)
+	if request.method == "POST":
+		order.delete()
+		return redirect('/')
+
+	context = {'order':order}
+	return render(request, 'accounts/delete.html', context)
