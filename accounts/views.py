d2ddee2ac0829ba4ef5739636da801d46ac197a4
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-08-11 12:12:47 +0530

    Added inline formsets for customers placing multiple orders in one go

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
 
@@ -34,16 +35,20 @@ def customer(request, pk):
     return render(request, 'accounts/customer.html', context)
 
 def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(Customer, Order, fields=('product','status')) # (parentModel,... 
+    # ...childModel, fields allowed from child model )
     customer =Customer.objects.get(id=pk)
-    form = OrderForm(initial={'customer':customer})
+    formset = OrderFormSet(instance=customer)
+    # form = OrderForm(initial={'customer':customer})
     if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect('/')
 
 
-    context = {'form': form }
+    context = {'formset': formset }
     return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
