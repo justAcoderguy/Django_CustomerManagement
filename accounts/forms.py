43b29b9e5b9c1b7255ce75f8084f30f8806a4093
Abhishek Banerjee <abhishek.banerjee18@outlook.com>
2020-08-12 20:38:54 +0530

    Added login and register page

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,8 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
 from .models import Order
 
 
@@ -7,3 +11,8 @@ class OrderForm(ModelForm):
         model = Order # Tells us which model this form is built for
         fields = '__all__' # Saying that create a form with all of Order Models fields otherwise use a list ['']
         
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 # Create your views here.
- 
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+            
+    context ={'form':form}
+    return render(request, "accounts/register.html", context)
+
+def loginPage(request):
+    context = {}
+    return render(request, "accounts/login.html", context)
+
+
+
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
